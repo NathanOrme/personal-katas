505e21f1c5c5a31e1fbb77238d839accf8b71792
Nathan Orme <nathan.orme23@gmail.com>
2024-07-22 21:29:59 +0100

    Worked On FizzBuzz

--- src/main/java/org/personal/katas/fizzbuzz/FizzBuzz.java
+++ src/main/java/org/personal/katas/fizzbuzz/FizzBuzz.java
@@ -2,10 +2,29 @@ package org.personal.katas.fizzbuzz;
 
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+import java.util.stream.Stream;
+
 @NoArgsConstructor
 public class FizzBuzz {
 
     public static String checkNumber(final int number) {
-        return "Fizz";
+        if (number % 15 == 0) {
+            return "FizzBuzz";
+        } else if (number % 5 == 0) {
+            return "Buzz";
+        } else if (number % 3 == 0) {
+            return "Fizz";
+        } else {
+            return String.valueOf(number);
+        }
+    }
+
+
+    public static List<String> checkRangeOfNumbers(final int number) {
+        return Stream.iterate(1, n -> n + 1)
+                .limit(number)
+                .map(FizzBuzz::checkNumber)
+                .toList();
     }
 }
--- src/test/java/org/personal/katas/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/org/personal/katas/fizzbuzz/FizzBuzzTest.java
@@ -2,13 +2,34 @@ package org.personal.katas.fizzbuzz;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class FizzBuzzTest {
+class FizzBuzzTest {
 
     @Test
     void checkNumber_With3_ReturnsFizz() {
         assertEquals("Fizz", FizzBuzz.checkNumber(3));
+    }
+
+    @Test
+    void checkNumber_With5_ReturnsFizz() {
+        assertEquals("Buzz", FizzBuzz.checkNumber(5));
+    }
+
+    @Test
+    void checkNumber_With15_ReturnsFizz() {
+        assertEquals("FizzBuzz", FizzBuzz.checkNumber(15));
+    }
+
+    @Test
+    void checkRangeOfNumbers_With15_ReturnsCorrectArray() {
+        List<String> expectedArray = List.of("1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz");
+        List<String> results = FizzBuzz.checkRangeOfNumbers(15);
+        for (int i = 0; i < expectedArray.size(); i++) {
+            assertEquals(expectedArray.get(i), results.get(i));
+        }
     }
 
 }
