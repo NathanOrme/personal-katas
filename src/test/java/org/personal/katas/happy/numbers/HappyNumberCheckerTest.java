e4a2abef90ea583ad74cd33efb034d0383930e57
Nathan Orme <nathan.orme23@gmail.com>
2024-07-22 18:57:01 +0100

    Worked On HappyNumberChecker

--- src/test/java/org/personal/katas/happy/numbers/HappyNumberCheckerTest.java
+++ src/test/java/org/personal/katas/happy/numbers/HappyNumberCheckerTest.java
@@ -2,19 +2,29 @@ package org.personal.katas.happy.numbers;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class HappyNumberCheckerTest {
 
     @Test
     void check_5_returnsFalse() {
-        assertFalse(HappyNumberChecker.checkIfHappyNumber(5));
+        assertFalse(HappyNumberChecker.isHappyNumber(5));
     }
 
     @Test
     void check_23_returnsTrue() {
-        assertTrue(HappyNumberChecker.checkIfHappyNumber(23));
+        assertTrue(HappyNumberChecker.isHappyNumber(23));
+    }
+
+    @Test
+    void getAllHappyNumbersInRange_With23_ReturnsList() {
+        List<Integer> integers = HappyNumberChecker.getAllHappyNumbersInRange(23);
+        assertNotNull(integers);
+        assertFalse(integers.isEmpty());
+        List<Integer> expectedIntegers = List.of(1, 7, 10, 13, 19, 23);
+        assertTrue(integers.containsAll(expectedIntegers));
     }
 
 }
