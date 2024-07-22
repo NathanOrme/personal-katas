7622650cb0bee9e52f5e57d6d1187ccd1d54c1e7
Nathan Orme <nathan.orme23@gmail.com>
2024-07-22 17:41:59 +0100

    Added Happy Numbers Kata

---
+++ src/test/java/org/personal/katas/happy/numbers/HappyNumberCheckerTest.java
@@ -0,0 +1,20 @@
+package org.personal.katas.happy.numbers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class HappyNumberCheckerTest {
+
+    @Test
+    void check_5_returnsFalse() {
+        assertFalse(HappyNumberChecker.checkIfHappyNumber(5));
+    }
+
+    @Test
+    void check_23_returnsTrue() {
+        assertTrue(HappyNumberChecker.checkIfHappyNumber(23));
+    }
+
+}
