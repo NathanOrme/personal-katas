dee8e603d822bfb23f74d7211adfad562905a4c0
Nathan Orme <nathan.orme23@gmail.com>
2024-07-22 17:34:26 +0100

    Added Perfect Numbers Kata

---
+++ src/test/java/org/personal/katas/perfect/numbers/PerfectNumberCheckerTest.java
@@ -0,0 +1,30 @@
+package org.personal.katas.perfect.numbers;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PerfectNumberCheckerTest {
+
+    @Test
+    void check_5_returnsFalse() {
+        assertFalse(PerfectNumberChecker.checkIfPerfectNumber(5));
+    }
+
+    @Test
+    void check_6_returnsTrue() {
+        assertTrue(PerfectNumberChecker.checkIfPerfectNumber(6));
+    }
+
+    @Test
+    void getAllPerfectNumbersInRange_WithNumber28_ExpectTwoNumbers() {
+        List<Integer> result = PerfectNumberChecker.getAllPerfectNumbersInRange(28);
+        assertFalse(result.isEmpty());
+        assertEquals(2, result.size());
+        assertTrue(result.containsAll(Arrays.asList(6, 28)));
+    }
+
+}
