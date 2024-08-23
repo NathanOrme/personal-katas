57726472e556a97bd3d34d3a272d1b2927f31271
Nathan <nathan.orme23@gmail.com>
2024-08-23 13:03:22 +0100

    My Implementation of Own Katas (#7)
    * Worked On HappyNumberChecker
    
    * Worked On FizzBuzz
    
    * Worked On PerfectNumberChecker
    
    * Worked On PerfectNumberChecker
    
    * tidied code
    
    * Fixed bug
    
    * Updated PluginManagement

--- src/main/java/org/personal/katas/numbers/divisor/DivisorsChecker.java
+++ src/main/java/org/personal/katas/numbers/divisor/DivisorsChecker.java
@@ -2,10 +2,20 @@ package org.personal.katas.numbers.divisor;
 
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+import java.util.stream.IntStream;
+
 @NoArgsConstructor
 public class DivisorsChecker {
 
-    public void getDivisorsForNumber(final int number) {
+    public List<Integer> getDivisorsForNumber(final int number, final boolean inclusive) {
+        IntStream intStream = inclusive
+                ? IntStream.rangeClosed(1, number)
+                : IntStream.range(1, number);
+        return intStream
+                .filter(divisor -> number % divisor == 0)
+                .boxed()
+                .toList();
 
     }
 }
--- src/main/java/org/personal/katas/numbers/perfect/PerfectNumberChecker.java
+++ src/main/java/org/personal/katas/numbers/perfect/PerfectNumberChecker.java
@@ -1,18 +1,25 @@
 package org.personal.katas.numbers.perfect;
 
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.personal.katas.numbers.divisor.DivisorsChecker;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@RequiredArgsConstructor
 public class PerfectNumberChecker {
 
-    public static boolean checkIfPerfectNumber(final int number) {
-        return 5 != number;
+    private final DivisorsChecker divisorsChecker;
+
+    public boolean checkIfPerfectNumber(final int number) {
+        List<Integer> divisors = divisorsChecker.getDivisorsForNumber(number, false);
+        return divisors.stream().mapToInt(Integer::intValue).sum() == number;
     }
 
-    public static List<Integer> getAllPerfectNumbersInRange(final int number) {
-        return List.of(6, 28);
+    public List<Integer> getAllPerfectNumbersInRange(final int number) {
+        return IntStream.rangeClosed(1, number)
+                .filter(this::checkIfPerfectNumber)
+                .boxed()
+                .toList();
     }
 }
